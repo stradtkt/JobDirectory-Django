0be2817cd53177b8355154de313b9730bd542c53
Kevin Stradtman <stradtkt@gmail.com>
2018-10-23 08:42:14 -0400

    Added an admin portion to the site

--- apps/jobs/admin.py
+++ apps/jobs/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'username')
+    search_fields = ('first_name', 'last_name', 'username')
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'budget', 'pay_type', 'length', 'created_at')
+    search_fields = ('title', 'company', 'length', 'budget')
+    date_hierarchy = 'created_at'
+
+class UserAppliedAdmin(admin.ModelAdmin):
+    list_display = ('job', 'user', 'amount_charged', 'cover_letter')
+    search_fields = ('job', 'user')
+    raw_id_fields = ('job', 'user')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(UserApplied, UserAppliedAdmin)
