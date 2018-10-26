82d631d3b000378663ffb172f6d203c805278ee3
Kevin Stradtman <stradtkt@gmail.com>
2018-10-26 17:46:54 -0400

    Changed the views and made a single post view

--- apps/jobs/filters.py
+++ apps/jobs/filters.py
@@ -15,6 +15,14 @@ BUDGET_CHOICES = (
     (5000, '5,000'),
     (10000, '10,000'),
     (20000, '20,000'),
+    (30000, '30,000'),
+    (40000, '40,000'),
+    (50000, '50,000'),
+    (60000, '60,000'),
+    (70000, '70,000'),
+    (80000, '80,000'),
+    (90000, '90,000'),
+    (100000, '100,000'),
 )
 class JobFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
